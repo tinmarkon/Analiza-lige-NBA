c83bbb3dbf81aa8989101d0f6fc826af9e0f8c0d
Tin Markon <tin.markon@stroka.si>
2021-10-27 15:21:17 +0200

    Dodana funkcija branja, zacetek vzorca za igralca

--- obdelava_podatkov.py
+++ obdelava_podatkov.py
@@ -1,5 +1,28 @@
 import os
 import re
 
+directory = "C:/Users/TinM/Documents/Studij/Programiranje 1/Analiza-lige-NBA"
+testna_datoteka = "testni.html"
 
-sample_player = re.compile(r'.*?data-stat="player" csk=(?P<Naziv>') ### TODO zaključi statistike igralcev
+
+def read_file(directory, filename):
+    """Funkcija najde datoteko in jo vrne kot niz"""
+    with open(os.path.join(directory, filename), 'r', encoding="utf-8") as f:
+        string = f.read()
+    return string
+
+
+sample_player = re.compile( r'<tr class="full_table" >.*?data-stat="player" csk="(?P<Igralec>\S*)"'
+                            r'.*?<td class="center " data-stat="pos" >(?P<Pozicija>\S*)</td>'
+                            r'',
+                            flags=re.DOTALL) 
+
+def find_players(niz, vzorec):
+    match = re.findall(vzorec, niz)
+    return (match, len(match))
+    
+
+testna_datoteka = read_file(directory, testna_datoteka)
+
+
+print(find_players(testna_datoteka, sample_player))
