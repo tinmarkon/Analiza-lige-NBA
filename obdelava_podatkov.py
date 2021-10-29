b06c07588c836e536d2502cb38353e0a94e01980
Tin Markon <tin.markon@stroka.si>
2021-10-29 20:51:34 +0200

    Zaključen vzuorec za igralca

--- obdelava_podatkov.py
+++ obdelava_podatkov.py
@@ -14,10 +14,18 @@ def read_file(directory, filename):
 
 sample_player = re.compile( r'<tr class="full_table" >.*?data-stat="player" csk="(?P<Igralec>\S*)"'
                             r'.*?<td class="center " data-stat="pos" >(?P<Pozicija>\S*)</td>'
-                            r'',
+                            r'.*?<td class="right non_qual" data-stat="fg_per_g" >(?P<FG>\S*)</td>'
+                            r'<td class="right non_qual" data-stat="fga_per_g" >(?P<FGA>\S*)</td>'
+                            r'.*?<td class="right non_qual" data-stat="trb_per_g" >(?P<Skoki>\S*)</td>'
+                            r'<td class="right non_qual" data-stat="ast_per_g" >(?P<Asistence>\S*)</td>'
+                            r'<td class="right non_qual" data-stat="stl_per_g" >(?P<Vkradene_žoge>\S*)</td>'
+                            r'<td class="right non_qual" data-stat="blk_per_g" >(?P<Blokade>\S*)</td>'
+                            r'<td class="right non_qual" data-stat="tov_per_g" >(?P<Izgubljene_žoge>\S*)</td>'
+                            r'.*?<td class="right non_qual" data-stat="pts_per_g" >(?P<Tocke>\S*)</td>',
                             flags=re.DOTALL) 
 
 def find_players(niz, vzorec):
+    """Funckija vzame niz in vzorec podatkov in vrača podatke o igralcih"""
     match = re.findall(vzorec, niz)
     return (match, len(match))
     
